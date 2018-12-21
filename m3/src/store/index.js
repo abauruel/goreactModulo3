cb7ca1dce93ce869990c7cc39c2d21ef001a5e9c
Alex Claude <abauruel@gmail.com>
2018-12-21 21:19:18 -0200

    adicionado config redux saga

--- m3/src/store/index.js
+++ m3/src/store/index.js
@@ -1,9 +1,19 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
 
 import reducers from './reducers';
+import sagas from './sagas';
+
+const Middleware = [];
+const sagaMiddleware = createSagaMiddleware();
+
+Middleware.push(sagaMiddleware);
+
 
 const createAppropriateStore = process.env.NODE_ENV === 'development' ? console.tron.createStore : createStore;
 
-const store = createAppropriateStore(reducers, compose(applyMiddleware(...[])));
+const store = createAppropriateStore(reducers, compose(applyMiddleware(...Middleware)));
+sagaMiddleware.run(sagas);
 
 export default store;

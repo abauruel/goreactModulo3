963bef44f831eccf4ead347faf33e850c97b3a39
Alex Claude <abauruel@gmail.com>
2018-12-22 00:45:16 -0200

    adicionado validação de repositorios existentes

--- m3/src/store/sagas/favorites.js
+++ m3/src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import api from '../../services/api';
 
 import { Creators as FavoriteActions } from '../ducks/favorites';
@@ -6,14 +6,19 @@ import { Creators as FavoriteActions } from '../ducks/favorites';
 export function* addFavorite(action) {
   try {
     const { data } = yield call(api.get, `/repos/${action.payload.repository}`);
+    const isDuplicated = yield select(state => state.favorites.data.find(favorite => favorite.id === data.id));
 
-    const repositoryData = {
-      id: data.id,
-      name: data.full_name,
-      description: data.description,
-      url: data.html_url,
-    };
-    yield put(FavoriteActions.addFavoritesSuccess(repositoryData));
+    if (isDuplicated) {
+      yield put(FavoriteActions.addFavoritesFailure('Repositorio ja existente!'));
+    } else {
+      const repositoryData = {
+        id: data.id,
+        name: data.full_name,
+        description: data.description,
+        url: data.html_url,
+      };
+      yield put(FavoriteActions.addFavoritesSuccess(repositoryData));
+    }
   } catch (error) {
     yield put(FavoriteActions.addFavoritesFailure('Error ao adicionar repositório'));
   }
